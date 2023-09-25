1fc382fb95846b931ada0d09d2eafd19f7d3960d
XianzeCao <xcao@coveo.com>
2023-09-24 23:48:00 -0400

    Apriori algorithm implementation

--- Apriori/src/Apriori.java
+++ Apriori/src/Apriori.java
@@ -1,2 +1,13 @@
 public class Apriori {
+    private static final double percentageThreshold = 0.1;
+
+    private static final int datasize = 100000;
+
+    private static final int support = (int) (datasize * percentageThreshold);
+
+    public static void main(String[] args) {
+
+    }
+
+
 }
