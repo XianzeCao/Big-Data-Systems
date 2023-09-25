3672e182439f85367f2c7a452b4c3d40dbb42bcb
XianzeCao <xcao@coveo.com>
2023-09-25 00:00:59 -0400

    Apriori algorithm implementation

--- Apriori/src/Apriori.java
+++ Apriori/src/Apriori.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 public class Apriori {
     private static final double percentageThreshold = 0.1;
 
@@ -5,8 +7,9 @@ public class Apriori {
 
     private static final int support = (int) (datasize * percentageThreshold);
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new FileReader("data/retail.txt"));
+        BufferedWriter output = new BufferedWriter(new FileWriter("data/results.txt"));
     }
 
 
